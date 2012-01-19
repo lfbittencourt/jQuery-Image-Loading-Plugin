56b06859395bbf8b3cd4157624781688dd2d3b64
LF Bittencourt <github@lfbittencourt.com>
2012-01-19 10:36:05 -0200

    first commit

---
+++ js/jquery.imageLoading.js
@@ -0,0 +1,67 @@
+/**
+ * jQuery Image Loading Plugin 1.0
+ * http://www.lfbittencourt.com/jquery-image-loading-plugin
+ *
+ * Copyright (c) 2011 Luís Fernando Bittencourt
+ * Licensed under the MIT license:
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+(function($) {
+
+    // Default settings
+    var settings = {
+        containerClass: 'image-loading',
+        onError: null,
+        onLoad: null
+    };
+
+    $.fn.imageLoading = function(options) {
+
+        if (options) {
+            $.extend(settings, options);
+        }
+
+        return this.each(function() {
+            var container = '<div class="';
+
+            container += settings.containerClass;
+            container += '" style="width: ';
+            container += this.clientWidth;
+            container += 'px; height: ';
+            container += this.clientHeight;
+            container += 'px"></div>';
+
+            $(this).wrap(container);
+            $(this).hide();
+
+            if (!this.complete) {
+
+                $(this).load(function() {
+                    $(this).fadeIn().unwrap();
+
+                    if (typeof settings.onLoad === 'function') {
+                        settings.onLoad(this);
+                    }
+                });
+
+                $(this).error(function() {
+                    if (typeof settings.onError === 'function') {
+                        settings.onError(this, this.parentNode);
+                    }
+                });
+
+            } else {
+
+                $(this).fadeIn();
+
+                if (typeof settings.onLoad === 'function') {
+                    settings.onLoad(this);
+                }
+
+            }
+        });
+
+    };
+
+})(jQuery);
